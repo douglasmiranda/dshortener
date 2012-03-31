7a03874114539e4be4e69ba16fc3bf9a870bf728
Douglas Miranda <douglasmirandasilva@gmail.com>
2012-03-31 13:41:35 -0400

    teste para quando informar somente o valor 0 para codificar e obter o uuid curto

--- dshortener/encurtador/tests/uuid.py
+++ dshortener/encurtador/tests/uuid.py
@@ -6,6 +6,11 @@ class UUIDCurtoTest(TestCase):
     def setUp(self):
         self.uuid = UUIDCurto()
 
+    def test_deve_retornar_o_primeiro_caractere_do_alfabeto_passado_ao_codificar_0(self):
+        inteiro = 0
+        uuidcurto = self.uuid.codificar(inteiro)
+        self.assertEqual(self.uuid.alfabeto[0], uuidcurto)
+
     def test_deve_transformar_um_inteiro_em_uuid_e_retornar_ao_valor_original(self):
         inteiro = 11523562
         uuidcurto = self.uuid.codificar(inteiro)
