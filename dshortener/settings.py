761bb1ab2aa619d32a37018c856881bbcbc16fff
Douglas Miranda <douglasmirandasilva@gmail.com>
2012-03-31 16:44:29 -0400

    criando o estilo base e a barra do header

--- dshortener/settings.py
+++ dshortener/settings.py
@@ -70,6 +70,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    abspath('static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
